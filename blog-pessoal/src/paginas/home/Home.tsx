17298a09137c326576f8485be54320991c4aff1d
wowilliam <wo.william@hotmail.com>
2022-03-21 23:50:17 -0300

    Criação do Projeto

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+           <img src="https://imgur.com/t/gaming/EMeAobe" alt="Imagem inicial" className="img"  /> 
+        </>
+    ); 
+}
+
+export default Home;
